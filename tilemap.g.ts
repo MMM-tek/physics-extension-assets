e7e7f01229b12c7c6da3f6febb8527614a8d2a58
MMM-tek <mtmirallesflores@gmail.com>
2026-03-02 18:59:10 +0100

    Archivos iniciales del proyecto de MakeCode

---
+++ tilemap.g.ts
@@ -0,0 +1,98 @@
+// Código generado automáticamente. No editar.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "1 --RAMPS":
+            case "tile1":return tile1;
+            case "2 --RAMPS":
+            case "tile3":return tile3;
+            case "3 --RAMPS":
+            case "tile4":return tile4;
+            case "4 --RAMPS":
+            case "tile5":return tile5;
+            case "5 --RAMPS":
+            case "tile7":return tile7;
+            case "6 --RAMPS":
+            case "tile9":return tile9;
+            case "7 --RAMPS":
+            case "tile10":return tile10;
+            case "8 --RAMPS":
+            case "tile8":return tile8;
+            case "9 --RAMPS":
+            case "tile17":return tile17;
+            case "10 --RAMPS":
+            case "tile21":return tile21;
+            case "11 --RAMPS":
+            case "tile14":return tile14;
+            case "12 --RAMPS":
+            case "tile19":return tile19;
+            case "13 --RAMPS":
+            case "tile2":return tile2;
+            case "14 --RAMPS":
+            case "tile11":return tile11;
+            case "1 --PHYSICS":
+            case "tile6":return tile6;
+            case "2 --PHYSICS":
+            case "tile12":return tile12;
+            case "3 --PHYSICS":
+            case "tile13":return tile13;
+            case "4 --PHYSICS":
+            case "tile15":return tile15;
+            case "5 --PHYSICS":
+            case "tile16":return tile16;
+            case "stairs --PHYSICS":
+            case "tile18":return tile18;
+            case "flag --PHYSICS":
+            case "tile20":return tile20;
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
